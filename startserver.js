177e21cf47c5936f254ba509871ae182430213b7
Beke András <beke.andras.mkolc@gmail.com>
2020-09-21 14:06:17 +0200

    req.url === changed

--- startserver.js
+++ startserver.js
@@ -1,4 +1,3 @@
-
 var mysql = require('mysql');
 var http = require('http');
 var url = require('url');
@@ -11,9 +10,9 @@ var db = mysql.createConnection({
   database: "palinkatrend"
 });
 
-db.connect(function(err) {
+db.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");  
+  console.log("Connected!");
 });
 
 function getDomain() {
@@ -23,22 +22,22 @@ function getDomain() {
 // * Important promise function
 function dbQuery(databaseQuery) {
   return new Promise(data => {
-      db.query(databaseQuery, function (error, result) { // change db->connection for your code
-          if (error) {
-              console.log(error);
-              throw error;
-          }
-          try {
-              console.log(result);
-              
-              data(result);
+    db.query(databaseQuery, function (error, result) { // change db->connection for your code
+      if (error) {
+        console.log(error);
+        throw error;
+      }
+      try {
+        console.log(result);
 
-          } catch (error) {
-              data({});
-              throw error;
-          }
+        data(result);
 
-      });
+      } catch (error) {
+        data({});
+        throw error;
+      }
+
+    });
   });
 
 }
@@ -46,17 +45,23 @@ function dbQuery(databaseQuery) {
 //create a server object:
 http.createServer(function (req, res) {
   var queryresult = null;
-    res.writeHead(200, {'Content-Type': 'text/html'});
-  
-    if (req.url == '/datas') {
-      queryresult = getDomain();
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  if (req.url === '/datas') {
+    queryresult = getDomain();
+  } else {
+    if (req.url === '/times') {
+
     } else {
+
     }
-    if (queryresult != null)
-    {
-      res.end(queryresult[0]);
-    } else {
-      res.end('Query succeed.');
-    }
-  
-    }).listen(3000); //the server object listens on port 3000
+  }
+  if (queryresult != null) {
+    res.end(queryresult[0]);
+  } else {
+    res.end('Query succeed.');
+  }
+
+}).listen(3000); //the server object listens on port 3000
